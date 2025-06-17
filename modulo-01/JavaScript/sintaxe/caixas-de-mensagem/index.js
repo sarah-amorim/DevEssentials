38318fb2a197124a73448dda0879b07dd2efa2b8
sarah <sarahamorim.050@gmail.com>
2025-06-17 19:35:49 -0300

    Update

--- modulo-01/JavaScript/sintaxe/caixas-de-mensagem/index.js
+++ modulo-01/JavaScript/sintaxe/caixas-de-mensagem/index.js
@@ -1,6 +1,19 @@
 // Alert 
 // A caixa de mensagem alert, como o próprio nome sugere, exibe um alerta para o usuário. 
-function alerta(){
-    alert("Seja bem-vindo ao site!")
+function exemplo_alert(){
+    alert("Seja bem-vindo ao site!");
 }
 
+// Prompt
+// Além de exibir uma mensagem, exibe também uma caixa de texto para coletar alguma informação do usuário
+function exemplo_prompt() {
+    let nome = prompt("Qual é o seu nome?");
+    document.writeln(nome + ", seja bem-vindo ao site!");
+}
+
+// Confirm
+//  Como o próprio nome sugere, serve para efetuarmos confirmações
+function exemplo_confirm() {
+    let opcao = confirm("Deseja encerrar?");
+    document.writeln("Sua escolha foi: " + opcao);
+}
