83db8241e42d4bdcb6ec85224bf87098d4a2694d
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-18 07:25:17 -0300

    Update loops.js

--- modulo-01/JavaScript/condicionais-loop/loops.js
+++ modulo-01/JavaScript/condicionais-loop/loops.js
@@ -14,6 +14,6 @@ while (contador <= 10){ // condição
 // Essa estrutura do loop for não permite, por exemplo, que esqueçamos de adicionar o incremento, evitando os loops infinitos. 
 for (contador = 1; contador <= 10; contador++) {// variável de controle - condição - contador ++ 
   console.log(
-    tabualda, 'x', contador, '=', tabuada * contador
+    tabuada, 'x', contador, '=', tabuada * contador
   );
 }
